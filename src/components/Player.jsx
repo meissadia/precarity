d26c3f847333bd305c05bc4fe76c1d0f47207aae
meis <meissadia@gmail.com>
2019-05-31 15:03:46 -0600

    Player score update animation

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { db } from '../firebase/firebase';
 import { isEqual } from 'lodash';
+
+import { db } from '../firebase/firebase';
 
 /**
  * Firebase Connected Player
@@ -9,22 +10,41 @@ export class Player extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            updated: false,
             player: {
                 id: props.player
-            }
-        }
-    }
+            },
+        };
+    };
 
     componentDidUpdate(prevProps, prevState) {
-        if (!isEqual(prevState.player, this.state.player)) {
+        /* Different Player */
+        if (!isEqual(prevState.player.id, this.state.player.id)) {
             this.unlisten();
             this.unlisten = db.collection('users').doc(this.state.player.id)
                 .onSnapshot(snap =>
                     this.setState({ player: snap.data().player })
-                )
+                );
+            return;
         };
 
-    }
+        /* Just a score update */
+        if (!isEqual(prevState.player.score, this.state.player.score)) {
+            this.setState({ updated: true })
+            return;
+        }
+
+        /* Not updated, remove updated flag and animation class */
+        if (this.state.updated) {
+            setTimeout(() => {
+                const element = document.getElementById(this.state.player.id);
+                let cname = element.getAttribute('class');
+                cname = cname.replace('flashGreen', '');
+                element.setAttribute('class', cname);
+                this.setState({ updated: false });
+            }, 1000);
+        }
+    };
 
     componentDidMount() {
         this.unlisten = db.collection('users').doc(this.state.player.id)
@@ -39,11 +59,15 @@ export class Player extends React.Component {
 
     render() {
         const { player } = this.state;
-        let cname = this.props.me ? 'me' : null;
-        cname = ['player', cname].join(' ');
+        let section_class;
+        section_class = [
+            'player',
+            this.props.me ? 'me' : null,
+            this.state.updated ? 'flashGreen' : null,
+        ].join(' ');
 
         return (
-            <div id={player.id} className={cname}>
+            <div id={player.id} className={section_class}>
                 <div className='indicator'>✦</div>
                 <h2 className='name'>{player.name}</h2>
                 <div className='score'>{player.score}</div>
--- src/components/Version.jsx
+++ src/components/Version.jsx
@@ -1,3 +1,3 @@
 import React from 'react';
 
-export const Version = () => <div id='version'>Version 0.1.3</div>;
+export const Version = () => <div id='version'>Version 0.1.4</div>;
